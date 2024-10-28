9ffe9175ce53bf426d082422c6d47062ec1bd769
gabymends <gabrielly.rosamends@gmail.com>
2024-10-28 10:18:21 -0300

    arrumando codigo

---
+++ pages/teladelogin/script.js
@@ -0,0 +1,13 @@
+function validateLogin() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const errorMessage = document.getElementById("error-message");
+
+    if (username === "" || password === "") {
+        errorMessage.style.display = "block";
+    } else {
+        errorMessage.style.display = "none";
+        window.location.href = "outratela.html";
+    }
+}
+
